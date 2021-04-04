781272459847ec29fd86197e503bf2e9c494d279
aminfauzi <aremean0107@gmail.com>
2021-04-04 08:42:44 +0000

    hlte: Revenge-ify
    Signed-off-by: aminfauzi <aremean0107@gmail.com>

--- revengeos_hlte.mk
+++ revengeos_hlte.mk
@@ -13,10 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common RevengeOS stuff.
+$(call inherit-product, vendor/revengeos/config/common.mk)
 
 $(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
 PRODUCT_DEVICE := hlte
-PRODUCT_NAME := lineage_hlte
+PRODUCT_NAME := revengeos_hlte
